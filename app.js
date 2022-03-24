f33fc3ef363d7a3e4a41a5f53532526ebad745bb
Giorgio Giuliani <giorgiogiuliani@C02FC10NML85.local>
2022-03-24 10:41:29 +0100

    post form

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 //initialize package
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 //
 const app = express();
 
-//middleware function
-app.use(express.urlencoded({extended: false})); 
-
-app.get('/', function(req,res){
-    res.send('<h1>ok</h1>');
-    console.log('pistol');
-});
+//middleware functions
+app.use(express.urlencoded({extended: false})); //for post - form
+app.use(express.static('public'));  //for static pages
 
 app.get('/about', function(req,res){
     const aboutPath = path.join(__dirname, 'views', 'about.html');
     res.sendFile(aboutPath)
-})
+});
 
 app.get('/confirm', function(req,res){
     const confirmPath = path.join(__dirname, 'views', 'confirm.html');
     res.sendFile(confirmPath)
-})
+});
 
-app.get('/index', function(req,res){
+app.get('/', function(req,res){
     const indPath = path.join(__dirname, 'views', 'index.html');
     res.sendFile(indPath)
-})
+});
 
 app.get('/recommend', function(req,res){
     const recPath = path.join(__dirname, 'views', 'recommend.html');
     res.sendFile(recPath)
-})
+});
+
+app.post('/recommend', function(req,res){
+    const restaurant = req.body;
+    const filePath = path.join(__dirname,'data','restaurants.js');
+    const fileData = fs.readFileSync(filePath);
+
+    const storedRestaurants = JSON.parse(fileData);
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+});
 
 app.get('/restaurants', function(req,res){
     const restPath = path.join(__dirname, 'views', 'restaurants.html');
     res.sendFile(restPath)
-})
+});
+
+
 
 
 
