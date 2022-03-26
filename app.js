ade139068cd716d17d00694cca56a525d5049b1b
Giorgio Giuliani <giorgiogiuliani@C02FC10NML85.local>
2022-03-26 11:35:01 +0100

    dynamic routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid');
 
 //
 const app = express();
@@ -31,6 +32,7 @@ app.get('/recommend', function(req,res){
 
 app.post('/recommend', function(req,res){
     const restaurant = req.body;
+    restaurant.id = uuid.v4();  //this doesn't exist yet but JS create it and assign the value
     const filePath = path.join(__dirname,'data','restaurants.json');
 
     const fileData = fs.readFileSync(filePath);
@@ -51,6 +53,22 @@ app.get('/restaurants', function(req,res){
 
     res.render('restaurants', {numberOfRest: storedRestaurants.length, restaurants: storedRestaurants});
 
+});
+
+app.get('/restaurants/:id', function(req, res){
+    const restaurantId = req.params.id;
+
+    //read the file
+    const filePath = path.join(__dirname,'data','restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    //look for restaurant with that ID 
+    for(const restaurant in storedRestaurants){
+        if (restaurant.id === restaurantId) {
+            return res.render('restaurant-detail', {restaurant: restaurant});
+        }
+    } 
 });
 
 //listen method to start listening on a certain port
